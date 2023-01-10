71ec7994e2070f08c6e2c3d4d0e227179990b577
sivagayathri <shivagayathrik2000@gmail.com>
2023-01-10 12:13:17 +0530

    miniprojects

---
+++ calc.js
@@ -0,0 +1,14 @@
+//display items
+function display(num){
+    result.value+=num
+    }
+    
+    function allclear(){
+        result.value=''
+    }
+    function equall(){
+        result.value = eval(result.value)
+    }
+    //function back(){
+       // result.value=result.value.slice(0,-1)
+    //}
